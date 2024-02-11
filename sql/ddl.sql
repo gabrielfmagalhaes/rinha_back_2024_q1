0f1c8a14e13af498816e0b3653cc4347a5f2b9fc
Gabriel Magalhães <magalhaes.gabrielf@gmail.com>
2024-02-11 12:08:47 -0300

    comment some tags and change tables

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -11,15 +11,15 @@ CREATE TABLE transacoes (
     tipo char(1) NOT NULL,
     valor integer NOT NULL,
     descricao varchar(10) NOT NULL,
-    efetuadaEm timestamp NOT NULL,
-    FOREIGN KEY (clienteId) REFERENCES clientes (id)
+    efetuadaEm timestamp NOT NULL
+    -- FOREIGN KEY (clienteId) REFERENCES clientes (id)
 );
 
 -- Se não rolar de manter uma ORM performatica
 -- alterar para queries isoladas com apoio de funções
 
--- CREATE INDEX fk_transacao_clienteid ON transacoes
--- (
---     clienteId ASC
--- );
+CREATE INDEX fk_transacao_clienteid ON transacoes
+(
+    clienteId ASC
+);
 
