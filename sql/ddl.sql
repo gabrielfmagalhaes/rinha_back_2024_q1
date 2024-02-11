f9b28c9f4902b5528f9210f5a05bc43105238d79
Gabriel Magalhães <magalhaes.gabrielf@gmail.com>
2024-02-11 00:37:34 -0300

    fix docker-compose and sql files

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -1,18 +1,19 @@
-CREATE TABLE cliente (
+CREATE TABLE clientes (
     id integer PRIMARY KEY NOT NULL,
+    nome varchar(25) NOT NULL,
     saldo integer NOT NULL,
     limite integer NOT NULL
 );
 
-CREATE TABLE transacao (
+CREATE TABLE transacoes (
     id SERIAL PRIMARY KEY,
-    clienteId integer NOT NULL
+    clienteId integer NOT NULL,
     valor integer NOT NULL,
     descricao varchar(10) NOT NULL,
-    efetuadaEm timestamp NOT NULL,
+    efetuadaEm timestamp NOT NULL
 );
 
-CREATE INDEX fk_transacao_clienteid ON transacao
+CREATE INDEX fk_transacao_clienteid ON transacoes
 (
     clienteId ASC
 );
--- sql/reset.sql
+++ sql/reset.sql
@@ -1,7 +1,7 @@
-INSERT INTO clientes (nome, saldo, limite)
+INSERT INTO clientes (id, nome, saldo, limite)
   VALUES
-    ('o barato sai caro', 0, 1000 * 100),
-    ('zan corp ltda', 0, 800 * 100),
-    ('les cruders', 0, 10000 * 100),
-    ('padaria joia de cocaia', 0, 100000 * 100),
-    ('kid mais', 0, 5000 * 100);
+    (1, 'o barato sai caro', 0, 1000 * 100),
+    (2, 'zan corp ltda', 0, 800 * 100),
+    (3, 'les cruders', 0, 10000 * 100),
+    (4, 'padaria joia de cocaia', 0, 100000 * 100),
+    (5, 'kid mais', 0, 5000 * 100);
